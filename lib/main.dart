7a3227d8dd856f25bb60ec008d2ff471c5835cbe
NilsVinmann <nils@snappx.io>
2023-05-17 09:51:45 +0200

    further refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:snappx_quiz/routing/go_routing.dart';
+import 'package:snappx_quiz/routing/app_routes.dart';
 
 void main() => runApp(const ProviderScope(child: MyApp()));
 
