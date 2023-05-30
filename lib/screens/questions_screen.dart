732e88b82e684817f034d4e7b96ef43931dc6311
NilsVinmann <nils@snappx.io>
2023-05-30 10:54:29 +0200

    refactored to avoid mediaquery calls

--- lib/screens/questions_screen.dart
+++ lib/screens/questions_screen.dart
@@ -23,6 +23,9 @@ class _QuestionsScreenState extends ConsumerState<QuestionsScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final screenWidth = mediaQuery.size.width;
+    final screenHeight = mediaQuery.size.height;
     final questions = ref.watch(questionsProvider);
     final answers = ref.watch(answersProvider);
     final currentQuestionIndex = ref.watch(currentQuestionIndexProvider);
@@ -33,72 +36,84 @@ class _QuestionsScreenState extends ConsumerState<QuestionsScreen> {
 
     return Scaffold(
       appBar: buildCustomAppBar(context, currentQuestionIndex, questions, ref),
-      body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width * 0.4,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                padding: EdgeInsets.all(40),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.white.withOpacity(0.5),
-                    width: 3.0,
-                  ),
-                  borderRadius: BorderRadius.circular(60),
-                ),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 40),
-                      child: Text(
-                        questions[currentQuestionIndex],
-                        style: Theme.of(context).textTheme.bodyLarge,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(top: screenHeight * 0.06),
+          child: Center(
+            child: Container(
+              width: screenWidth * 0.4,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(screenWidth * 0.04),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.white.withOpacity(0.5),
+                        width: 3.0,
                       ),
+                      borderRadius: BorderRadius.circular(60),
                     ),
-                    ListView.separated(
-                      shrinkWrap: true,
-                      itemCount: answers[currentQuestionIndex].length,
-                      separatorBuilder: (context, index) => Divider(
-                        color: Theme.of(context).dividerColor,
-                      ),
-                      itemBuilder: (context, index) {
-                        final answer = answers[currentQuestionIndex][index];
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 12),
+                          child: Text(
+                            questions[currentQuestionIndex],
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                        ),
+                        ListView.separated(
+                          shrinkWrap: true,
+                          itemCount: answers[currentQuestionIndex].length,
+                          separatorBuilder: (context, index) => Divider(
+                            color: Theme.of(context).dividerColor,
+                          ),
+                          itemBuilder: (context, index) {
+                            final answer = answers[currentQuestionIndex][index];
 
-                        return RadioListTile(
-                          activeColor: Color(0xFF78FCB0),
-                          title: Text(answer,
-                              style: Theme.of(context).textTheme.bodyMedium),
-                          controlAffinity: ListTileControlAffinity.trailing,
-                          value: index,
-                          groupValue: selectedAnswer[currentQuestionIndex],
-                          onChanged: (value) {
-                            ref.read(selectedAnswerProvider.notifier).state =
-                                List.from(selectedAnswer)
+                            return RadioListTile(
+                              activeColor: Color(0xFF78FCB0),
+                              title: Text(answer,
+                                  style:
+                                      Theme.of(context).textTheme.bodyMedium),
+                              controlAffinity: ListTileControlAffinity.trailing,
+                              value: index,
+                              groupValue: selectedAnswer[currentQuestionIndex],
+                              onChanged: (value) {
+                                ref
+                                    .read(selectedAnswerProvider.notifier)
+                                    .state = List.from(selectedAnswer)
                                   ..[currentQuestionIndex] = value as int;
+                              },
+                            );
                           },
-                        );
-                      },
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              buildBottomButton(isAnswerSelected, isLastQuestion, context),
-            ],
+            ),
           ),
         ),
       ),
+      floatingActionButton:
+          buildBottomButton(isAnswerSelected, isLastQuestion, context),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 
   Padding buildBottomButton(
       bool isAnswerSelected, bool isLastQuestion, BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final screenWidth = mediaQuery.size.width;
+    final screenHeight = mediaQuery.size.height;
     return Padding(
-      padding: const EdgeInsets.only(top: 40),
+      padding: EdgeInsets.only(bottom: screenHeight * 0.08),
       child: SizedBox(
-        width: double.infinity,
-        height: 60,
+        width: screenWidth * 0.4,
+        height: screenHeight * 0.08,
         child: ElevatedButton(
           onPressed: isAnswerSelected
               ? () {
@@ -114,7 +129,7 @@ class _QuestionsScreenState extends ConsumerState<QuestionsScreen> {
             style: TextStyle(
                 fontFamily: 'SF Pro Text',
                 fontWeight: FontWeight.w600,
-                fontSize: 18),
+                fontSize: screenWidth * 0.013),
           ),
         ),
       ),
@@ -123,9 +138,14 @@ class _QuestionsScreenState extends ConsumerState<QuestionsScreen> {
 
   AppBar buildCustomAppBar(BuildContext context, int currentQuestionIndex,
       List<String> questions, WidgetRef ref) {
+    final mediaQuery = MediaQuery.of(context);
+    final screenWidth = mediaQuery.size.width;
+    final screenHeight = mediaQuery.size.height;
     return AppBar(
       leading: currentQuestionIndex > 0
           ? Container(
+              height: screenHeight * 0.03,
+              width: screenWidth * 0.03,
               padding: EdgeInsets.all(8),
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
@@ -140,15 +160,14 @@ class _QuestionsScreenState extends ConsumerState<QuestionsScreen> {
                 },
                 icon: Icon(
                   Icons.arrow_back,
-                  size: 24,
                   color: Color(0xFF78FCB0),
                 ),
               ),
             )
           : null,
       title: SizedBox(
-        height: 30,
-        width: MediaQuery.of(context).size.width * 0.4,
+        height: screenHeight * 0.03,
+        width: screenWidth * 0.4,
         child: Stack(
           children: <Widget>[
             Center(
@@ -162,9 +181,9 @@ class _QuestionsScreenState extends ConsumerState<QuestionsScreen> {
             Center(
               child: Container(
                 color: Colors.black,
-                width: MediaQuery.of(context).size.width * 0.08,
+                width: screenWidth * 0.08,
                 alignment: Alignment.center,
-                padding: EdgeInsets.symmetric(horizontal: 16),
+                padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.01),
                 child: Text(
                   '${currentQuestionIndex + 1}/${questions.length}',
                   style: Theme.of(context).appBarTheme.titleTextStyle,
@@ -179,57 +198,62 @@ class _QuestionsScreenState extends ConsumerState<QuestionsScreen> {
   }
 
   void showDialogOnAppStart(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final screenWidth = mediaQuery.size.width;
+    final screenHeight = mediaQuery.size.height;
     showDialog(
       barrierDismissible: false,
       context: context,
       builder: (_) => BackdropFilter(
         filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-        child: AlertDialog(
-          insetPadding: EdgeInsets.zero,
-          contentPadding: EdgeInsets.zero,
-          clipBehavior: Clip.antiAliasWithSaveLayer,
-          backgroundColor: Colors.grey.withOpacity(0.2),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(100)),
-          ),
-          content: Builder(
-            builder: (context) {
-              var height = MediaQuery.of(context).size.height;
-              var width = MediaQuery.of(context).size.width;
-
-              return Container(
-                height: height - 100,
-                width: width - 100,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 100),
-                      child: Text(
-                        'Which technology is\nbest for your app?',
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.displayLarge,
-                      ),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.3,
-                      height: 60,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
+        child: Stack(children: [
+          AlertDialog(
+            insetPadding: EdgeInsets.zero,
+            contentPadding: EdgeInsets.zero,
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            backgroundColor: Colors.grey.withOpacity(0.2),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(100)),
+            ),
+            content: Builder(
+              builder: (context) {
+                return Container(
+                  height: screenHeight - (screenHeight * 0.1),
+                  width: screenWidth - (screenWidth * 0.07),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(bottom: screenWidth * 0.1),
                         child: Text(
-                          'Find it out',
-                          style: Theme.of(context).textTheme.labelLarge,
+                          'Which technology is\nbest for your app?',
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context).textTheme.displayLarge,
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              );
-            },
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: SizedBox(
+                          width: screenWidth * 0.4,
+                          height: screenHeight * 0.08,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text(
+                              'Find it out',
+                              style: Theme.of(context).textTheme.labelLarge,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
           ),
-        ),
+        ]),
       ),
     );
   }
